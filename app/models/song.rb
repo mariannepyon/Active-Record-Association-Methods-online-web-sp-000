c1d9a0a0963a782238a54347ca855c5b77080768
Marianne Pyon <mariannepyon@gmail.com>
2019-10-16 18:38:33 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
@@ -10,8 +12,12 @@ class Song < ActiveRecord::Base
     # when this method is called it should assign the song's artist to Drake
     # Drake doesn't exist in the database as an artist yet, so you'll have to create a record
     # Hint: you won't want to create an artist record every time this method is called, only if an Drake is *not found*
-    self.artist = Artist.new.create(name: "Drake")
-
-
+    artist = Artist.find_by(name: "Drake")
+    #binding.pry
+    if artist
+      self.artist = artist
+    else
+      self.artist = Artist.create(name: "Drake")
+    end
   end
 end
